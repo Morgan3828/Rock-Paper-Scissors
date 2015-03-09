c936e84922a70d24b80dd2cb041a5a56d665321e
Morgan3828 <morgan5091@gmail.com>
2015-03-09 09:12:27 -0700

    Second rearrangement

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -14,19 +14,18 @@ OPTIONS = ( None,
             "Paper",
             "Scissors")
 
-# Conditional Execution
 # While Loop
 
 while answer != "no" and answer != "n":    
     userchoice = int( input("  1 for Rock, 2 for Paper, 3 for Scissors. " +
                                 " Rock, Paper, or Scissors ? :"))
+# Conditional Execution
     computer = random.randrange(len(OPTIONS))
     print("The computer chose {}.".format( OPTIONS[computer]))
         
     if computer == 1 and userchoice == 1:
-        simpledialog
-        "It's a tie"
-
+        tkinter.simpledialog("It's a tie")
+        
     answer = input("Do you want to play again (y/n)? ")
     answer = answer.lower()[0]
 
