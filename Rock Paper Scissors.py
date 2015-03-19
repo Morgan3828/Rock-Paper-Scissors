dc5ed8b2526f56c455bc3c430333b2791f07f34e
Morgan3828 <morgan5091@gmail.com>
2015-03-19 09:10:34 -0700

    Put in if statement framework

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -19,13 +19,51 @@ OPTIONS = ( None,
 while answer != "no" and answer != "n":    
     userchoice = int( input("  1 for Rock, 2 for Paper, 3 for Scissors. " +
                                 " Rock, Paper, or Scissors ? :"))
-# Conditional Execution
+# Randomization AI
     computer = random.randrange(len(OPTIONS))
     print("The computer chose {}.".format( OPTIONS[computer]))
         
+# Tie section
     if computer == 1 and userchoice == 1:
-        tkinter.simpledialog("It's a tie")
-        
+        print("It's a Tie! Nobody wins.")
+
+    if computer == 2 and userchoice == 2:
+        print("It's a Tie! Nobody wins.")
+
+    if computer == 3 and userchoice == 3:
+        print("It's a Tie! Nobody wins.")
+
+# Loss Section
+    if computer == 1 and userchoice == 3:
+        print("You Lose.")
+
+    if computer == 3 and userchoice == 2:
+        print("You Lose.")
+
+    if computer == 2 and userchoice == 1:
+        print("You Lose.")
+
+# Win section
+    if computer == 1 and userchoice == 2:
+        print("You Win!")
+
+    if computer == 2 and userchoice == 3:
+        print("You Win!")
+
+    if computer == 3 and userchoice == 1:
+        print("You Win!")
+
+# None Section
+    if computer == 0 and userchoice == 1:
+        print("The computer didn't pick anything... Sorry bout that...")
+
+    if computer == 0 and userchoice == 2:
+        print("The computer didn't pick anything... Sorry bout that...")
+
+    if computer == 0 and userchoice == 3:
+        print("The computer didn't pick anything... Sorry bout that...")
+
+# While loop end        
     answer = input("Do you want to play again (y/n)? ")
     answer = answer.lower()[0]
 
