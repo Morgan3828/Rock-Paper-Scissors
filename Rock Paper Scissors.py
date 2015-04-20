33f64a279910a5fc8bebd0a39afa17a049963cd9
Morgan3828 <morgan5091@gmail.com>
2015-04-20 09:06:13 -0700

    Completed program

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -20,50 +20,63 @@ w.pack()
 
 # While Loop
 
-while answer != 2:    
-    userchoice = simpledialog.askinteger("  1 for Rock, 2 for Paper, 3 for Scissors. ",
-                                " Rock, Paper, or Scissors ? :"))
+while answer != 2:     
+    userchoice = simpledialog.askinteger(" Rock, Paper, or Scissors? :",
+                                         "1 for Rock, 2 for Paper, 3 for Scissors")
 # Randomization AI
     computer = random.randrange(len(OPTIONS))
-    messagebox.showinfo("The computer chose {}.".format( OPTIONS[computer]))
+    messagebox.showinfo("Rock, Paper, Scissors",
+                        "The computer chose {}.".format( OPTIONS[computer]))
         
 # Tie section
     if computer == 1 and userchoice == 1:
-        print("It's a Tie! Nobody wins.")
+        messagebox.showinfo("Rock, Paper, Scissors",
+                            "It's a Tie! Nobody wins.")
 
     if computer == 2 and userchoice == 2:
-        print("It's a Tie! Nobody wins.")
+        messagebox.showinfo("Rock, Paper, Scissors",
+                            "It's a Tie! Nobody wins.")
 
     if computer == 3 and userchoice == 3:
-        print("It's a Tie! Nobody wins.")
+        messagebox.showinfo("Rock, Paper, Scissors",
+                            "It's a Tie! Nobody wins.")
 
 # Loss Section
     if computer == 1 and userchoice == 3:
-        print("You Lose.")
+        messagebox.showinfo("Rock, Paper, Scissors",
+                            "You Lose.")
 
     if computer == 3 and userchoice == 2:
-        print("You Lose.")
+        messagebox.showinfo("Rock, Paper, Scissors",
+                            "You Lose.")
 
     if computer == 2 and userchoice == 1:
-        print("You Lose.")
+        messagebox.showinfo("Rock, Paper, Scissors",
+                            "You Lose.")
 
 # Win section
     if computer == 1 and userchoice == 2:
-        print("You Win!")
+        messagebox.showinfo("Rock, Paper, Scissors",
+                            "You Win!")
 
     if computer == 2 and userchoice == 3:
-        print("You Win!")
+        messagebox.showinfo("Rock, Paper, Scissors",
+                            "You Win!")
 
     if computer == 3 and userchoice == 1:
-        print("You Win!")
+        messagebox.showinfo("Rock, Paper, Scissors",
+                            "You Win!")
 
 # None Section
-    else:
-            answer = 2
+    if computer == None and userchoice == 1:		
+        answer = 1		                
+		
+    if computer == None and userchoice == 2:		
+        answer = 1		
+		
+    if computer == None and userchoice == 3:
+        answer = 1
                 
 # While loop end        
     answer = simpledialog.askinteger("Do you want to play again (y/n)? ",
                                      "1 = Yes 2 = No")
-    answer = answer.lower()[0]
-
-                          
